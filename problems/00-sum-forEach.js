425b0891a0ec5dc621e124fd0e86e51d2eda7943
Ruben Cuello <kuelmusic@gmail.com>
2024-03-30 14:49:25 -0400

    00

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -6,15 +6,20 @@ Solve this using Array's `forEach()` method.
 
 Examples:
 
-console.log(sumForEach([4, 3, -1, 10])); // 16
-console.log(sumForEach([6, 7, 2])); // 15
-console.log(sumForEach([])); // 0
+
 */
 
 let sumForEach = function(arr) {
-  // Your code here 
+  let sum = 0
+  arr.forEach((num) => {
+    sum += num
+  })
+  return sum
 };
 
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
