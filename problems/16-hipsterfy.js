acd60bcfda7e0c9e1a2ae07802da0d3f19d7bf82
Ruben Cuello <kuelmusic@gmail.com>
2024-03-31 19:35:37 -0400

    16

--- problems/16-hipsterfy.js
+++ problems/16-hipsterfy.js
@@ -31,8 +31,8 @@ let hipsterfy = function(sentence) {
 };
 
 console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
-// console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
-// console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
