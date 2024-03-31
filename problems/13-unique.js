ad58c60ac5d1616fbc9d1b6998b92fc0ee7f9616
tylerwebster <webstertyler81@yahoo.com>
2024-03-31 18:25:45 -0400

    13 solved

--- problems/13-unique.js
+++ problems/13-unique.js
@@ -5,15 +5,51 @@ duplicates.
 Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
 methods.
 Examples:
-console.log(unique([1, 1, 2, 3, 3])); // [1, 2, 3]
-console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
-console.log(unique(['a', 'b', 'c', 'b'])); // ['a', 'b', 'c']
+
 */
 
 let unique = function(array) {
-    // Your code here 
+    // Your code here
+    let arr = []
+    array.filter((ele)=>{
+        if(!arr.includes(ele)){
+
+            arr.push(ele)
+        }
+    })
+    return arr
 };
 
+// let unique = function(array) {
+//     let newArr = array.sort()
+//     // console.log(newArr)
+//     let arr = []
+//     newArr.filter((ele)=>{
+//         if(arr[arr.length -1] !== ele){
+
+//             arr.push(ele)
+//         }
+//     })
+//     return arr
+// };
+
+
+// let unique = function(array) {
+//     // Your code here
+//     let arr = []
+//     array.filter((ele)=>{
+//         if(arr.includes(ele) === false){
+
+//             arr.push(ele)
+//         }
+//     })
+//     return arr
+// };
+
+
+console.log(unique([1, 1, 2, 3, 3])); // [1, 2, 3]
+console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
+console.log(unique(['a', 'b', 'c', 'b'])); // ['a', 'b', 'c']
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
